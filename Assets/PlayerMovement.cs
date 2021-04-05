1cbd22847a5cee97e9bdb8e6a7bfb4c0478aa464
seojinseojin <1106laura@naver.com>
2021-04-05 14:55:03 +0900

    [Feat] 배경 추가

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float speed = 30f;
 
     // Start is called before the first frame update
     void Start()
